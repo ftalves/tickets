891741d44683c8edc277606241dc65405c9cefb3
ftalves <ftelles.bus@gmail.com>
2021-07-04 16:48:39 -0300

    Move app to a separate file

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,43 +1,8 @@
 import 'module-alias/register';
 import 'express-async-errors';
-import express from 'express';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
 
-import { errorHandler, currentUser } from './middlewares';
-import {
-  currentUserRouter,
-  signInRouter,
-  signOutRouter,
-  signUpRouter,
-} from './routes';
-import { NotFoundError } from './errors';
-
-const app = express();
-
-app.set('trust proxy', 1); // trust first proxy
-
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-app.use(currentUser);
-
-app.use(currentUserRouter);
-app.use(signInRouter);
-app.use(signOutRouter);
-app.use(signUpRouter);
-
-app.all('*', () => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
