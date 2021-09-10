87b334fc86bfae1d9b0b07c3d0dbb66c034046d5
ftalves <ftelles.bus@gmail.com>
2021-09-10 12:37:21 -0300

    Add request to signup form

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,12 +1,23 @@
 import { useState } from 'react';
+import axios from 'axios';
 
-export default () => {
+const Signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    alert(JSON.stringify({ email, password }));
+
+    try {
+      const response = await axios.post('/api/users/signup', {
+        email,
+        password,
+      });
+      setErrors([]);
+    } catch (err) {
+      setErrors(err.response.data);
+    }
   };
 
   return (
@@ -23,13 +34,25 @@ export default () => {
       <div className="form-group">
         <label>Password</label>
         <input
-          type="password"
+          type="current-password"
           className="form-control"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
       </div>
       <button className="btn btn-primary">Sign Up</button>
+      {errors.length ? (
+        <div className="alert alert-danger">
+          <h4>Ooops...</h4>
+          <ul className="my-0">
+            {errors.map((err) => (
+              <li key={err.msg}>{err.msg}</li>
+            ))}
+          </ul>
+        </div>
+      ) : null}
     </form>
   );
 };
+
+export default Signup;
