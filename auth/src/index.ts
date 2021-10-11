853da2938482dbfd9bcc4fe4e737d53248be22ca
ftalves <ftelles.bus@gmail.com>
2021-10-10 21:11:07 -0300

    Add mongo URI as env variables

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -10,7 +10,7 @@ const start = async () => {
   }
 
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+    await mongoose.connect(process.env.MONGO_URI || '', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -10,7 +10,7 @@ const start = async () => {
   }
 
   try {
-    await mongoose.connect('mongodb://tickets-mongo-srv:27017/auth', {
+    await mongoose.connect(process.env.MONGO_URI || '', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
